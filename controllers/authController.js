aa1ae4fe1e39df2629f6ede9f1d3fbb2c9093452
Hemanth939281 <hkh777650@gmail.com>
2025-02-15 12:40:37 +0530

    changed auth logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,14 @@
 export const login = async (req, res) => {
     const { username, password } = req.body;
-    console.log(username, password);
+
+    if (!username ||!password) {
+      return res.status(400).json({ message: 'Missing username or password' });
+    }
     
     if (username === "teacher" && password === "teacher123") {
       console.log(process.env.USERNAME, process.env.PASSWORD);
       return res.status(200).json({ message: 'Login successful' });
     }
-    
-    if (!username ||!password) {
-      return res.status(400).json({ message: 'Missing username or password' });
-    }
-
   
     res.status(401).json({ message: 'Invalid credentials' });
   };
--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,6 +1,7 @@
-const errorMiddleware = (err, req, res, next) => {
+const errorMiddleware = (err,res) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
+
   };
   
   export default errorMiddleware;
